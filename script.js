1dd2171dc9c96c20a4058a67e896f6f3b5b0216f
Rodrigo <ronico041012@gmail.com>
2022-03-28 23:00:56 -0300

    about section styled

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const doc = document.querySelector('body');
 const tag = document.querySelector('.width');
 
+const logo = document.querySelector('.bottom')
+
+const svgDoc = logo.contentDocument;
+
+console.log(svgDoc)
+const svg = svgDoc.getElementById('logo');
+console.log(svg)
 window.addEventListener('resize', (value) => {
     tag.innerHTML = window.innerWidth
 })
