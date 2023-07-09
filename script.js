32e43afcb2aa01119f7c38206e75956ac2a9e8d4
Rodrigo Reynaga <ronico041012@gmail.com>
2023-07-08 21:18:39 -0300

    update style

--- script.js
+++ script.js
@@ -3,11 +3,37 @@ const tag = document.querySelector('.width');
 
 const logo = document.querySelector('.bottom')
 
-const svgDoc = logo.contentDocument;
+const menuIcon = document.querySelector('.menu-icon');
 
-console.log(svgDoc)
-const svg = svgDoc.getElementById('logo');
-console.log(svg)
+const nav = document.querySelector('nav ul');
+
+const navContainer = document.querySelector('.top')
+
+document.addEventListener("scroll", (e)=>
+    {
+        if(window.scrollY > 0 && !navContainer.classList.contains("nav-shadow")){
+            navContainer.classList.add('nav-shadow');
+        }
+        if (window.scrollY == 0) {
+            navContainer.classList.remove('nav-shadow');
+        }
+    });
+
+
+
 window.addEventListener('resize', (value) => {
     tag.innerHTML = window.innerWidth
 })
+
+console.log(menuIcon);
+
+menuIcon.addEventListener('click',
+    () => {
+        if(nav.classList.contains('menu-active')){
+            nav.classList.remove("menu-active");
+        }
+        else{
+            nav.classList.add("menu-active");
+        }
+} 
+);
